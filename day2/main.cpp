bbca66b7e8ecc660953d8d54dc047c3c154c71cb
evref <isak.lefevre@hotmail.se>
2023-12-02 09:56:38 +0100

    implement day 2, puzzle 1

---
+++ day2/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+void line_possible(bool* possible, string line) {
+    int lim_red = 12, lim_green = 13, lim_blue = 14;
+    int red = 0, green = 0, blue = 0;
+
+    bool last_num = false;
+    int temp_num = 0;
+    string buffer = "";
+    
+    // Start at i=8 to ignore "Game X: "
+    for (int i = 8; i < line.length()+1; i++) {
+        if (isdigit(line[i])) {
+            last_num = true;
+            buffer += line[i];
+        } else if (!isdigit(line[i]) && last_num) {
+            last_num = false;
+            temp_num = stoi(buffer);
+            buffer = "";
+        } else if (line[i] == 'r') {
+            red = temp_num;
+            temp_num = 0;
+            i += 2;
+        } else if (line[i] == 'g') {
+            green = temp_num;
+            temp_num = 0;
+            i += 4;
+        } else if (line[i] == 'b') {
+            blue = temp_num;
+            temp_num = 0;
+            i += 3;
+        } else if (line[i] == ';' || i == line.size()) {
+            if (red > lim_red || green > lim_green || blue > lim_blue) {
+                *possible = false;
+                break;
+            } else *possible = true;
+
+            red = 0;
+            green = 0;
+            blue = 0;
+            i += 1;
+        }
+    }
+}
+
+int main() {
+    string input_data_path = "input";
+
+    fstream file;
+    string line;
+    file.open(input_data_path);
+
+    int total = 0;
+    if (file.is_open()) {
+        int i = 1;
+        while (getline(file, line)) {
+            bool possible;
+            line_possible(&possible, line);
+            if (possible) total += i;
+            i++;
+        }
+    } else {
+        throw invalid_argument("ERROR: Input file could not be opened");
+    }
+
+    cout << to_string(total) << endl;
+
+    return 0;
+}
