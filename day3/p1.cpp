a512af977331e59a42b189ce5efc549b01a2898d
evref <isak.lefevre@hotmail.se>
2023-12-03 19:01:47 +0100

    even more refactoring

--- day3/p1.cpp
+++ day3/p1.cpp
@@ -13,19 +13,15 @@ void symbol_line(bool* is_adjacent, int i, string line, bool is_cur_line = false
     }
 }
 
-void symbol_adjacent(bool* is_adjacent, int i, string line, string last_line, string next_line) {
-    if (last_line != "") symbol_line(is_adjacent, i, last_line);
-    if (next_line != "") symbol_line(is_adjacent, i, next_line);
-    symbol_line(is_adjacent, i, line, true);
-}
-
 void compute_line_p1(int* total, string line, string last_line, string next_line) {
     string buffer = "";
     bool is_adjacent = false;
     for (int i = 0; i < line.length(); i++) {
         if (isdigit(line[i])) {
             buffer += line[i];
-            symbol_adjacent(&is_adjacent, i, line, last_line, next_line);
+            if (last_line != "") symbol_line(&is_adjacent, i, last_line);
+            if (next_line != "") symbol_line(&is_adjacent, i, next_line);
+            symbol_line(&is_adjacent, i, line, true);
         }
         if ((!isdigit(line[i]) && buffer != "") || (i == line.size()-1 && buffer != "")) {
             if (is_adjacent) *total += stoi(buffer);
