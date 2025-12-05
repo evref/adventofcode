a8d232e695597a59728bd570aa8def4cdb9b72ac
evref <isak.lefevre@hotmail.se>
2025-12-05 22:53:49 +0100

    refactor: create function for finding largest num idx in string

--- 2025/day3/Main.java
+++ 2025/day3/Main.java
@@ -31,30 +31,10 @@ public class Main {
         int sum = 0;
 
         for (int i = 0; i < inputData.length; i++) {
-            int maxValueIdx = 0;
-            int maxValue = 0;
-
             // Check every element but the last, needs to be a two digit number
-            for (int j = 0; j < inputData[i].length()-1; j++) {
-                int currentValue = inputData[i].charAt(j);
+            int maxValueIdx = findLargestNumInStringIdx(inputData[i], 0, inputData[i].length()-1);
 
-                if (maxValue < currentValue) {
-                    maxValue = currentValue;
-                    maxValueIdx = j;
-                }
-            }
-
-            int secondMaxValueIdx = 0;
-            int secondMaxValue = 0;
-
-            for (int j = maxValueIdx+1; j < inputData[i].length(); j++) {
-                int currentValue = inputData[i].charAt(j);
-
-                if (secondMaxValue < currentValue) {
-                    secondMaxValue = currentValue;
-                    secondMaxValueIdx = j;
-                }
-            }
+            int secondMaxValueIdx = findLargestNumInStringIdx(inputData[i], maxValueIdx+1, inputData[i].length());
 
             String number = "" + inputData[i].charAt(maxValueIdx) + inputData[i].charAt(secondMaxValueIdx);
 
@@ -62,5 +42,21 @@ public class Main {
         }
 
         return sum;
+    }
+
+    static int findLargestNumInStringIdx(String string, int startIdx, int endIdx) {
+        int maxValue = 0;
+        int maxValueIdx = 0;
+
+        for (int j = startIdx; j < endIdx; j++) {
+            int currentValue = string.charAt(j);
+
+            if (maxValue < currentValue) {
+                maxValue = currentValue;
+                maxValueIdx = j;
+            }
+        }
+
+        return maxValueIdx;
     }
 }
